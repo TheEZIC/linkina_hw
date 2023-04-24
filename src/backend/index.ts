ad764982a84bcc9c510d74d62d8d11b5af488b8d
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2023-04-25 00:10:02 +0300

    improve typing

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,7 +1,7 @@
 import { Database, open } from "sqlite";
 import sqlite3 from "sqlite3";
 
-var database: Database;
+let database: Database;
 
 export default {
   initDatabase() {
@@ -10,23 +10,26 @@ export default {
       driver: sqlite3.Database,
     }).then(_ => database = _);
   },
-
-  login(username: string, password: string): Pick<User, 'id' | 'role' | 'name'> {
-    return database.get(`SELECT id, role, name FROM users WHERE username = ? AND password = ?`, [username, password]) as any;
+  login(username: string, password: string): Promise<Pick<User, 'id' | 'role' | 'name'> | undefined> {
+    return database.get(`SELECT id, role, name FROM users WHERE username = ? AND password = ?`, [username, password]);
   },
   register(username: string, password: string, name: string) {
     return database.exec(`INSERT INTO users (role, username, password, name) VALUES (0, ?, ?, ?)`, [...arguments]);
   },
   async getContactInfo(id: number) {
     let user = await database.get(`SELECT contact_info_id`);
-    // 
+    //
   },
   async updateContactInfo(id: number, update: Partial<Omit<ContactInfo, 'id'>>) {
-    let keys = Object.keys(update);
+    let keys = Object.keys(update) as (keyof Partial<Omit<ContactInfo, 'id'>>)[];
     let info = await database.get(`SELECT * FROM contact_info WHERE id = ?`, [id]) as ContactInfo;
-    if(!info)
-      return database.run(`INSERT INTO contact_info (${keys.join(', ')})`, keys.map(v => (update as any)[v]));
-    else
-      return database.run(`UPDATE contact_info SET ${keys.map(v => `${v} = ?`).join(', ')} WHERE id = ?`, [...keys.map(v => (update as any)[v]), id]);
+
+    if(!info) {
+      return database.run(`INSERT INTO contact_info (${keys.join(', ')})`, keys.map(v => update[v]));
+    } else {
+      let params = [...keys.map((v) => update[v]), id];
+
+      return database.run(`UPDATE contact_info SET ${keys.map(v => `${v} = ?`).join(', ')} WHERE id = ?`, params);
+    }
   },
 }
