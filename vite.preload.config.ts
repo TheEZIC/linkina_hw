dc37d58e75acfb1d758f039e9777f96c5fbcbebe
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2023-04-24 22:00:09 +0300

    add vite ts config paths

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import paths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [paths()],
+});
